newaetech/chipwhisperer_full_backup

commit 474d3a6eee241a7eba66a5e4533f0c0ee31b7f69

cw312 xc7a35 target: add example AES+ECC projects.
Also update corresponding notebooks and CW305.py to support the new
FPGA target transparently.

hardware/victims/cw305_artixtarget/fpga/common/cw305_top.v
@@ -38,6 +38,7 @@ module cw305_top #(
 )(
     // USB Interface
     input wire                          usb_clk,        // Clock
+    output wire                         usb_clk_buf,    // if needed by parent module
     inout wire [7:0]                    usb_data,       // Data for write/read
     input wire [pADDR_WIDTH-1:0]        usb_addr,       // Address
     input wire                          usb_rdn,        // !RD, low when addr valid for read
@@ -89,7 +90,6 @@ module cw305_top #(
     wire crypt_done;
     wire crypt_busy;
 
-    wire usb_clk_buf;
     wire [7:0] usb_dout;
     wire isout;
     wire [pADDR_WIDTH-pBYTECNT_SIZE-1:0] reg_address;

hardware/victims/cw305_artixtarget/fpga/vivado_examples/ecc_p256_pmul/hdl/cw305_ecc_p256_pmul_top.v
@@ -38,6 +38,7 @@ module cw305_ecc_p256_pmul_top #(
 )(
     // USB Interface
     input wire                          usb_clk,        // Clock
+    output wire                         usb_clk_buf,    // Clock
     inout wire [7:0]                    usb_data,       // Data for write/read
     input wire [pADDR_WIDTH-1:0]        usb_addr,       // Address
     input wire                          usb_rdn,        // !RD, low when addr valid for read
@@ -86,7 +87,6 @@ module cw305_ecc_p256_pmul_top #(
     wire crypt_done;
     wire crypt_busy;
 
-    wire usb_clk_buf;
     wire [7:0] usb_dout;
     wire isout;
     wire [pADDR_WIDTH-pBYTECNT_SIZE-1:0] reg_address;

hardware/victims/cw308_ufo_target/xc7a35/vivado/ss2_cw305_aes.xpr
@@ -3,10 +3,10 @@
 <!--                                                         -->
 <!-- Copyright 1986-2022 Xilinx, Inc. All Rights Reserved.   -->
 
-<Project Version="7" Minor="60" Path="/home/jpnewae/git/cw_develop/hardware/victims/cw308_ufo_target/xc7a35/vivado/ss2.xpr">
+<Project Version="7" Minor="60" Path="/home/jpnewae/git/cw_develop/hardware/victims/cw308_ufo_target/xc7a35/vivado/ss2_cw305_aes.xpr">
   <DefaultLaunch Dir="$PRUNDIR"/>
   <Configuration>
-    <Option Name="Id" Val="aa7e6de7d2014d26a9722fc24226ec96"/>
+    <Option Name="Id" Val="947e506fd3494bf3b269679308a2bb3c"/>
     <Option Name="Part" Val="xc7a35tcsg324-1"/>
     <Option Name="CompiledLibDir" Val="$PCACHEDIR/compile_simlib"/>
     <Option Name="CompiledLibDirXSim" Val=""/>
@@ -87,6 +87,41 @@
   <FileSets Version="1" Minor="31">
     <FileSet Name="sources_1" Type="DesignSrcs" RelSrcDir="$PSRCDIR/sources_1" RelGenDir="$PGENDIR/sources_1">
       <Filter Type="Srcs"/>
+      <File Path="$PPRDIR/../../../cw305_artixtarget/fpga/cryptosrc/aes_googlevault/aes_core.v">
+        <FileInfo>
+          <Attr Name="UsedIn" Val="synthesis"/>
+          <Attr Name="UsedIn" Val="implementation"/>
+          <Attr Name="UsedIn" Val="simulation"/>
+        </FileInfo>
+      </File>
+      <File Path="$PPRDIR/../../../cw305_artixtarget/fpga/cryptosrc/aes_googlevault/aes_ks.v">
+        <FileInfo>
+          <Attr Name="UsedIn" Val="synthesis"/>
+          <Attr Name="UsedIn" Val="implementation"/>
+          <Attr Name="UsedIn" Val="simulation"/>
+        </FileInfo>
+      </File>
+      <File Path="$PPRDIR/../../../cw305_artixtarget/fpga/cryptosrc/aes_googlevault/aes_sbox.v">
+        <FileInfo>
+          <Attr Name="UsedIn" Val="synthesis"/>
+          <Attr Name="UsedIn" Val="implementation"/>
+          <Attr Name="UsedIn" Val="simulation"/>
+        </FileInfo>
+      </File>
+      <File Path="$PPRDIR/../../../cw305_artixtarget/fpga/common/cdc_pulse.v">
+        <FileInfo>
+          <Attr Name="UsedIn" Val="synthesis"/>
+          <Attr Name="UsedIn" Val="implementation"/>
+          <Attr Name="UsedIn" Val="simulation"/>
+        </FileInfo>
+      </File>
+      <File Path="$PPRDIR/../../../cw305_artixtarget/fpga/common/clocks.v">
+        <FileInfo>
+          <Attr Name="UsedIn" Val="synthesis"/>
+          <Attr Name="UsedIn" Val="implementation"/>
+          <Attr Name="UsedIn" Val="simulation"/>
+        </FileInfo>
+      </File>
       <File Path="$PPRDIR/../hdl/crc.v">
         <FileInfo>
           <Attr Name="UsedIn" Val="synthesis"/>
@@ -94,21 +129,28 @@
           <Attr Name="UsedIn" Val="simulation"/>
         </FileInfo>
       </File>
-      <File Path="$PPRDIR/../hdl/cw305_reg_test_ss2.v">
+      <File Path="$PPRDIR/../../../cw305_artixtarget/fpga/common/cw305_defines.v">
         <FileInfo>
           <Attr Name="UsedIn" Val="synthesis"/>
           <Attr Name="UsedIn" Val="implementation"/>
           <Attr Name="UsedIn" Val="simulation"/>
         </FileInfo>
       </File>
-      <File Path="$PPRDIR/../hdl/cw305_ss2_example_top.v">
+      <File Path="$PPRDIR/../../../cw305_artixtarget/fpga/common/cw305_reg_aes.v">
         <FileInfo>
           <Attr Name="UsedIn" Val="synthesis"/>
           <Attr Name="UsedIn" Val="implementation"/>
           <Attr Name="UsedIn" Val="simulation"/>
         </FileInfo>
       </File>
-      <File Path="$PPRDIR/../hdl/cw305_usb_reg_fe.v">
+      <File Path="$PPRDIR/../../../cw305_artixtarget/fpga/common/cw305_top.v">
+        <FileInfo>
+          <Attr Name="UsedIn" Val="synthesis"/>
+          <Attr Name="UsedIn" Val="implementation"/>
+          <Attr Name="UsedIn" Val="simulation"/>
+        </FileInfo>
+      </File>
+      <File Path="$PPRDIR/../../../cw305_artixtarget/fpga/common/cw305_usb_reg_fe.v">
         <FileInfo>
           <Attr Name="UsedIn" Val="synthesis"/>
           <Attr Name="UsedIn" Val="implementation"/>
@@ -136,7 +178,7 @@
           <Attr Name="UsedIn" Val="simulation"/>
         </FileInfo>
       </File>
-      <File Path="$PPRDIR/../hdl/ss2_wrapper.v">
+      <File Path="$PPRDIR/../hdl/ss2_aes_wrapper.v">
         <FileInfo>
           <Attr Name="UsedIn" Val="synthesis"/>
           <Attr Name="UsedIn" Val="implementation"/>
@@ -145,9 +187,9 @@
       </File>
       <Config>
         <Option Name="DesignMode" Val="RTL"/>
-        <Option Name="TopModule" Val="ss2_wrapper"/>
-        <Option Name="TopAutoSet" Val="TRUE"/>
-        <Define Name="SS2_EXAMPLE"/>
+        <Option Name="TopModule" Val="ss2_aes_wrapper"/>
+        <Option Name="VerilogDir" Val="$PPRDIR/../../../cw305_artixtarget/fpga/common"/>
+        <Define Name="GOOGLE_VAULT_AES"/>
       </Config>
     </FileSet>
     <FileSet Name="constrs_1" Type="Constrs" RelSrcDir="$PSRCDIR/constrs_1" RelGenDir="$PGENDIR/constrs_1">
@@ -159,7 +201,6 @@
         </FileInfo>
       </File>
       <Config>
-        <Option Name="TargetConstrsFile" Val="$PPRDIR/cw312_ss2.xdc"/>
         <Option Name="ConstrsType" Val="XDC"/>
       </Config>
     </FileSet>
@@ -167,7 +208,7 @@
       <Filter Type="Srcs"/>
       <Config>
         <Option Name="DesignMode" Val="RTL"/>
-        <Option Name="TopModule" Val="ss2_wrapper"/>
+        <Option Name="TopModule" Val="aes_core"/>
         <Option Name="TopLib" Val="xil_defaultlib"/>
         <Option Name="TopAutoSet" Val="TRUE"/>
         <Option Name="TransportPathDelay" Val="0"/>

hardware/victims/cw308_ufo_target/xc7a35/hdl/cw305_ss2_example_top.v
@@ -29,7 +29,7 @@ either expressed or implied, of NewAE Technology Inc.
 `timescale 1ns / 1ps
 `default_nettype none 
 
-module cw305_top #(
+module cw305_ss2_example_top #(
     parameter pBYTECNT_SIZE = 8,
     parameter pADDR_WIDTH = 32
 )(

hardware/victims/cw308_ufo_target/xc7a35/hdl/ss2.v
@@ -30,20 +30,18 @@ module ss2 #(
     parameter pPARITY_ENABLED = 0
 )(
     input  wire         clk,
-    output wire         clkout,
     input  wire         resetn,
     input  wire         rxd,
     output wire         txd,
 
-    output wire         led1,
-    output wire         led2,
-    output wire         led3,
-    output wire         io3,
-    output wire         io4
+    inout  wire [7:0]   dut_data,
+    output reg  [31:0]  dut_address,
+    output reg          dut_rdn,
+    output reg          dut_wrn,
+    output wire         dut_cen,
+    output reg          error
 );
-    //wire io3, io4;
 
-    wire clk_buf;
     wire rx_data_ready;
     wire [8:0] rx_data_word;
     reg  rx_ack;
@@ -84,20 +82,16 @@ module ss2 #(
     wire fifo_underflow;
 
 
-    reg [31:0] dut_address;
     reg rx_dut_cen;
     reg dut_cen_r;
-    reg dut_wrn;
-    reg dut_rdn;
     reg [7:0] dut_wdata;
-    wire [7:0] dut_data = dut_wrn? 8'bz : dut_wdata;
+    assign dut_data = dut_wrn? 8'bz : dut_wdata;
 
     reg [2:0] tx_state;
     reg tx_crc_init = 1'b0;
     reg tx_dut_cen;
 
-    reg  error = 1'b0;
-    always @ (posedge clk_buf) begin
+    always @ (posedge clk) begin
         if (~resetn)
             error <= 1'b0;
         else if ( rx_invalid_command || 
@@ -111,8 +105,6 @@ module ss2 #(
                 )
             error <= 1'b1;
     end
-
-    assign led2 = error;
 
     wire [7:0] rx_data_byte = rx_data_word[8:1];
     wire [7:0] rx_data = (rx_insert_zero)? 8'b0 : rx_data_byte;
@@ -134,7 +126,7 @@ module ss2 #(
 
     reg [7:0] dut_read_len;
 
-    always @ (posedge clk_buf) begin
+    always @ (posedge clk) begin
         dut_cen_r <= dut_cen;
         if (dut_cen && ~dut_cen_r)
             // note this handles incremeting the address for both the write (Rx) logic and read (Tx response) logic:
@@ -200,7 +192,7 @@ module ss2 #(
     end
 
     // Rx FSM: strategy is that FSM deals with COBS, not content
-    always @ (posedge clk_buf) begin
+    always @ (posedge clk) begin
         if (~resetn)
             rx_state <= pS_RX_IDLE;
         else begin
@@ -290,7 +282,7 @@ module ss2 #(
     reg dut_register_read_pre;
     reg dut_register_read;
 
-    always @ (posedge clk_buf) begin
+    always @ (posedge clk) begin
         tx_next_byte_r <= tx_next_byte;
         if (dut_register_read) dut_data_r <= dut_data;
         dut_register_read_pre <= ~dut_rdn && ~tx_dut_cen;
@@ -357,7 +349,7 @@ module ss2 #(
     reg clear_rx_errors = 1'b0;
 
     // Tx FSM: strategy is that FSM deals with COBS, not content
-    always @ (posedge clk_buf) begin
+    always @ (posedge clk) begin
         if (~resetn)
             tx_state <= pS_TX_IDLE;
         else begin
@@ -468,7 +460,7 @@ module ss2 #(
     wire crc_en = (rx_active)? rx_valid_byte : tx_next_byte_r;
     wire crc_init = (rx_active)? rx_crc_init : tx_crc_init;
     crc U_crc (
-        .clk            (clk_buf),
+        .clk            (clk),
         .init           (crc_init),
         .data           (crc_data),
         .data_en        (crc_en),
@@ -476,7 +468,7 @@ module ss2 #(
     );
 
     uart_core U_uart (
-       .clk                      (clk_buf),
+       .clk                      (clk),
        .reset_n                  (resetn),
        // Configuration inputs
        .bit_rate                 (pBIT_RATE),
@@ -498,7 +490,7 @@ module ss2 #(
        .txd_data                 (tx_data_byte),
        .txd_busy                 (tx_busy)
     );
-    always @(posedge clk_buf) rx_ack <= rx_syn;
+    always @(posedge clk) rx_ack <= rx_syn;
     assign rx_data_ready = rx_syn && rx_ack;
 
     fifo_sync #(
@@ -509,7 +501,7 @@ module ss2 #(
         .pBRAM                  (0),    // NOTE: consider switching to BRAM for implementation?
         .pDISTRIBUTED           (0)
     ) U_tx_fifo (
-        .clk                    (clk_buf              ),
+        .clk                    (clk                  ),
         .rst_n                  (resetn               ),
         .full_threshold_value   (                     ),
         .empty_threshold_value  (                     ),
@@ -527,46 +519,7 @@ module ss2 #(
         .underflow              (fifo_underflow       )
     );
 
-
-    wire dut_cen = (reading)? tx_dut_cen : rx_dut_cen;
-    cw305_top #(
-        .pBYTECNT_SIZE                  (8),
-        .pADDR_WIDTH                    (32)
-    ) U_cw305_dut (
-        .usb_clk                        (clk),
-        .usb_clk_buf                    (clk_buf),
-        .usb_data                       (dut_data),
-        .usb_addr                       (dut_address),
-        .usb_rdn                        (dut_rdn ),
-        .usb_wrn                        (dut_wrn ),
-        .usb_cen                        (dut_cen ),
-        .usb_trigger                    (1'b0),
-
-        .j16_sel                        (1'b0),
-        .k16_sel                        (1'b0),
-        .k15_sel                        (1'b0),
-        .l14_sel                        (1'b0),
-        .pushbutton                     (resetn),
-
-        .led1                           (led1),
-        .led2                           (),
-        .led3                           (led3),
-
-    `ifdef SS2_EXAMPLE
-        .io3                            (io3),
-        .ss2_error                      (error),
-    `endif
-
-        .pll_clk1                       (clk_buf),
-        .tio_trigger                    (io4),
-        .tio_clkout                     (clkout),
-        .tio_clkin                      (clk_buf)
-    );
-
-    `ifndef SS2_EXAMPLE
-        assign io3 = 1'bz;
-    `endif
-
+    assign dut_cen = (reading)? tx_dut_cen : rx_dut_cen;
 
 endmodule
 `default_nettype wire

hardware/victims/cw308_ufo_target/xc7a35/hdl/ss2_aes_wrapper.v
@@ -1,0 +1,103 @@
+`timescale 1 ns / 1 ps
+`default_nettype none
+
+/***********************************************************************
+This file is part of the ChipWhisperer Project. See www.newae.com for more
+details, or the codebase at http://www.chipwhisperer.com
+
+Copyright (c) 2023, NewAE Technology Inc. All rights reserved.
+Author: Jean-Pierre Thibault <jpthibault@newae.com>
+
+  chipwhisperer is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  chipwhisperer is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU Lesser General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with chipwhisperer.  If not, see <http://www.gnu.org/licenses/>.
+*************************************************************************/
+
+module ss2_aes_wrapper #(
+    parameter pBIT_RATE = 32  // number of clocks per UART bit; 230400 bps @ 7.37 MHz
+)(
+    input  wire         clk,
+    output wire         clkout,
+    input  wire         resetn,
+    input  wire         rxd,
+    output wire         txd,
+
+    output wire         led1,
+    output wire         led2,
+    output wire         led3,
+    output wire         io3,
+    output wire         io4
+);
+
+    wire [7:0] dut_data;
+    wire [31:0] dut_address;
+    wire dut_rdn;
+    wire dut_wrn;
+    wire dut_cen;
+    wire ss2_error;
+    wire clk_buf;
+    
+    assign led2 = ss2_error;
+
+    ss2 #(
+        .pBIT_RATE              (pBIT_RATE),
+        .pDATA_BITS             (8),
+        .pSTOP_BITS             (1),
+        .pPARITY_BITS           (0),
+        .pPARITY_ENABLED        (0)
+    ) U_ss2 (
+        .clk                    (clk_buf     ),
+        .resetn                 (resetn      ),
+        .rxd                    (rxd         ),
+        .txd                    (txd         ),
+
+        .dut_data               (dut_data    ),
+        .dut_address            (dut_address ),
+        .dut_rdn                (dut_rdn     ),
+        .dut_wrn                (dut_wrn     ),
+        .dut_cen                (dut_cen     ),
+        .error                  (ss2_error   )
+    );
+
+
+    cw305_top #(
+        .pBYTECNT_SIZE                  (8),
+        .pADDR_WIDTH                    (32)
+    ) U_cw305_dut (
+        .usb_clk                        (clk),
+        .usb_clk_buf                    (clk_buf),
+        .usb_data                       (dut_data),
+        .usb_addr                       (dut_address),
+        .usb_rdn                        (dut_rdn ),
+        .usb_wrn                        (dut_wrn ),
+        .usb_cen                        (dut_cen ),
+        .usb_trigger                    (1'b0),
+
+        .j16_sel                        (1'b0),
+        .k16_sel                        (1'b0),
+        .k15_sel                        (1'b0),
+        .l14_sel                        (1'b0),
+        .pushbutton                     (resetn),
+
+        .led1                           (led1),
+        .led2                           (),
+        .led3                           (led3),
+
+        .pll_clk1                       (clk_buf),
+        .tio_trigger                    (io4),
+        .tio_clkout                     (clkout),
+        .tio_clkin                      (clk_buf)
+    );
+
+endmodule
+`default_nettype wire
+

hardware/victims/cw308_ufo_target/xc7a35/hdl/ss2_ecc_wrapper.v
@@ -1,0 +1,105 @@
+`timescale 1 ns / 1 ps
+`default_nettype none
+
+/***********************************************************************
+This file is part of the ChipWhisperer Project. See www.newae.com for more
+details, or the codebase at http://www.chipwhisperer.com
+
+Copyright (c) 2023, NewAE Technology Inc. All rights reserved.
+Author: Jean-Pierre Thibault <jpthibault@newae.com>
+
+  chipwhisperer is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  chipwhisperer is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU Lesser General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with chipwhisperer.  If not, see <http://www.gnu.org/licenses/>.
+*************************************************************************/
+
+module ss2_ecc_wrapper #(
+    parameter pBIT_RATE = 32  // number of clocks per UART bit; 230400 bps @ 7.37 MHz
+)(
+    input  wire         clk,
+    output wire         clkout,
+    input  wire         resetn,
+    input  wire         rxd,
+    output wire         txd,
+
+    output wire         led1,
+    output wire         led2,
+    output wire         led3,
+    output wire         io3,
+    output wire         io4
+);
+
+    wire [7:0] dut_data;
+    wire [31:0] dut_address;
+    wire dut_rdn;
+    wire dut_wrn;
+    wire dut_cen;
+    wire ss2_error;
+    wire clk_buf;
+    
+    assign led2 = ss2_error;
+
+    ss2 #(
+        .pBIT_RATE              (pBIT_RATE),
+        .pDATA_BITS             (8),
+        .pSTOP_BITS             (1),
+        .pPARITY_BITS           (0),
+        .pPARITY_ENABLED        (0)
+    ) U_ss2 (
+        .clk                    (clk_buf     ),
+        .resetn                 (resetn      ),
+        .rxd                    (rxd         ),
+        .txd                    (txd         ),
+
+        .dut_data               (dut_data    ),
+        .dut_address            (dut_address ),
+        .dut_rdn                (dut_rdn     ),
+        .dut_wrn                (dut_wrn     ),
+        .dut_cen                (dut_cen     ),
+        .error                  (ss2_error   )
+    );
+
+
+    cw305_ecc_p256_pmul_top #(
+        .pBYTECNT_SIZE                  (8),
+        .pADDR_WIDTH                    (32)
+    ) U_cw305_dut (
+        .usb_clk                        (clk),
+        .usb_clk_buf                    (clk_buf),
+        .usb_data                       (dut_data),
+        .usb_addr                       (dut_address),
+        .usb_rdn                        (dut_rdn ),
+        .usb_wrn                        (dut_wrn ),
+        .usb_cen                        (dut_cen ),
+        .usb_trigger                    (1'b0),
+
+        .j16_sel                        (1'b0),
+        .k16_sel                        (1'b0),
+        .k15_sel                        (1'b0),
+        .l14_sel                        (1'b0),
+        .pushbutton                     (resetn),
+
+        .led1                           (led1),
+        .led2                           (),
+        .led3                           (led3),
+
+        .pll_clk1                       (clk_buf),
+        .tio_trigger                    (io4),
+        .tio_clkout                     (clkout),
+        .tio_clkin                      (clk_buf)
+    );
+
+    assign io3 = 1'bz;
+
+endmodule
+`default_nettype wire
+

commit e5a3d7a262ea31fc4570500c0f4175d384c8547d

Fix https://github.com/newaetech/chipwhisperer/issues/435
(inadvertently introduced by 474d3a6eee241a7eba66a5e4533f0c0ee31b7f69)

hardware/victims/cw305_artixtarget/fpga/common/cw305_top.v
@@ -38,7 +38,9 @@ module cw305_top #(
 )(
     // USB Interface
     input wire                          usb_clk,        // Clock
+`ifdef SS2_WRAPPER
     output wire                         usb_clk_buf,    // if needed by parent module
+`endif
     inout wire [7:0]                    usb_data,       // Data for write/read
     input wire [pADDR_WIDTH-1:0]        usb_addr,       // Address
     input wire                          usb_rdn,        // !RD, low when addr valid for read
@@ -80,6 +82,9 @@ module cw305_top #(
 `endif
     );
 
+`ifndef SS2_WRAPPER
+    wire usb_clk_buf;
+`endif
 
     wire [pKEY_WIDTH-1:0] crypt_key;
     wire [pPT_WIDTH-1:0] crypt_textout;

hardware/victims/cw305_artixtarget/fpga/vivado_examples/ecc_p256_pmul/hdl/cw305_ecc_p256_pmul_top.v
@@ -38,7 +38,9 @@ module cw305_ecc_p256_pmul_top #(
 )(
     // USB Interface
     input wire                          usb_clk,        // Clock
+`ifdef SS2_WRAPPER
     output wire                         usb_clk_buf,    // Clock
+`endif
     inout wire [7:0]                    usb_data,       // Data for write/read
     input wire [pADDR_WIDTH-1:0]        usb_addr,       // Address
     input wire                          usb_rdn,        // !RD, low when addr valid for read
@@ -80,6 +82,9 @@ module cw305_ecc_p256_pmul_top #(
 `endif
     );
 
+`ifndef SS2_WRAPPER
+    wire usb_clk_buf;
+`endif
 
     wire crypt_init;
     wire crypt_ready;

hardware/victims/cw308_ufo_target/xc7a35/vivado/ss2_cw305_aes.xpr
@@ -190,6 +190,7 @@
         <Option Name="TopModule" Val="ss2_aes_wrapper"/>
         <Option Name="VerilogDir" Val="$PPRDIR/../../../cw305_artixtarget/fpga/common"/>
         <Define Name="GOOGLE_VAULT_AES"/>
+        <Define Name="SS2_WRAPPER"/>
       </Config>
     </FileSet>
     <FileSet Name="constrs_1" Type="Constrs" RelSrcDir="$PSRCDIR/constrs_1" RelGenDir="$PGENDIR/constrs_1">
@@ -260,7 +261,9 @@
   <Runs Version="1" Minor="19">
     <Run Id="synth_1" Type="Ft3:Synth" SrcSet="sources_1" Part="xc7a35tcsg324-1" ConstrsSet="constrs_1" Description="Vivado Synthesis Defaults" AutoIncrementalCheckpoint="true" IncrementalCheckpoint="$PSRCDIR/utils_1/imports/synth_1/ss2.dcp" WriteIncrSynthDcp="false" State="current" Dir="$PRUNDIR/synth_1" IncludeInArchive="true" IsChild="false" AutoIncrementalDir="$PSRCDIR/utils_1/imports/synth_1" AutoRQSDir="$PSRCDIR/utils_1/imports/synth_1">
       <Strategy Version="1" Minor="2">
-        <StratHandle Name="Vivado Synthesis Defaults" Flow="Vivado Synthesis 2022"/>
+        <StratHandle Name="Vivado Synthesis Defaults" Flow="Vivado Synthesis 2022">
+          <Desc>Vivado Synthesis Defaults</Desc>
+        </StratHandle>
         <Step Id="synth_design"/>
       </Strategy>
       <GeneratedRun Dir="$PRUNDIR" File="gen_run.xml"/>
@@ -270,7 +273,9 @@
     </Run>
     <Run Id="impl_1" Type="Ft2:EntireDesign" Part="xc7a35tcsg324-1" ConstrsSet="constrs_1" Description="Default settings for Implementation." AutoIncrementalCheckpoint="false" WriteIncrSynthDcp="false" State="current" Dir="$PRUNDIR/impl_1" SynthRun="synth_1" IncludeInArchive="true" IsChild="false" GenFullBitstream="true" AutoIncrementalDir="$PSRCDIR/utils_1/imports/impl_1" AutoRQSDir="$PSRCDIR/utils_1/imports/impl_1">
       <Strategy Version="1" Minor="2">
-        <StratHandle Name="Vivado Implementation Defaults" Flow="Vivado Implementation 2022"/>
+        <StratHandle Name="Vivado Implementation Defaults" Flow="Vivado Implementation 2022">
+          <Desc>Default settings for Implementation.</Desc>
+        </StratHandle>
         <Step Id="init_design"/>
         <Step Id="opt_design"/>
         <Step Id="power_opt_design"/>

hardware/victims/cw308_ufo_target/xc7a35/vivado/ss2_cw305_ecc.xpr
@@ -434,6 +434,7 @@
         <Option Name="VerilogDir" Val="$PPRDIR/../../../cw305_artixtarget/fpga/cryptosrc/cryptech/ecdsa256-v1/rtl/curve"/>
         <Option Name="VerilogDir" Val="$PPRDIR/../../../cw305_artixtarget/fpga/cryptosrc/cryptech/ecdsa256-v1/rtl/lowlevel"/>
         <Option Name="VerilogDir" Val="$PPRDIR/../../../cw305_artixtarget/fpga/cryptosrc/cryptech/ecdsa256-v1/rtl/modular/modular_invertor"/>
+        <Define Name="SS2_WRAPPER"/>
       </Config>
     </FileSet>
     <FileSet Name="constrs_1" Type="Constrs" RelSrcDir="$PSRCDIR/constrs_1" RelGenDir="$PGENDIR/constrs_1">